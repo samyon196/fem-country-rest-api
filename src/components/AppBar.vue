c8c4b114980709676492f4a52a2fd2c692e96869
samyon196 <samyon.v@gmail.com>
2023-02-24 18:48:42 +0200

    wip: started working on app layout. think how to reduce all the calcs?

---
+++ src/components/AppBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-row justify-between items-center h-full">
+    <span class="text-lg">Where in the world?</span>
+    <span>Dark mode :)</span>
+  </div>
+</template>
+<script setup></script>
